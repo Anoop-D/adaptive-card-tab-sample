3b646fe383b86c6d150f3a62af82a1fb33a950d2
Anoop Dhillon <andhillo@microsoft.com>
2021-01-12 17:31:01 -0800

    remove unneeded tab code

--- src/app/acPrototypeBot/AcPrototypeBot.ts
+++ src/app/acPrototypeBot/AcPrototypeBot.ts
@@ -1,25 +1,16 @@
-import {
-  BotDeclaration,
-  MessageExtensionDeclaration,
-  IBot,
-  PreventIframe,
-} from "express-msteams-host";
+import { BotDeclaration, IBot, PreventIframe } from "express-msteams-host";
 import * as debug from "debug";
-import { DialogSet, DialogState } from "botbuilder-dialogs";
 import {
-  StatePropertyAccessor,
   CardFactory,
   TurnContext,
   MemoryStorage,
   ConversationState,
-  ActivityTypes,
   InvokeResponse,
 } from "botbuilder";
 import fetch from "node-fetch";
-import HelpDialog from "./dialogs/HelpDialog";
 import WelcomeCard from "./dialogs/WelcomeDialog";
 import VideoPlayerCard from "./dialogs/VideoPlayerCard";
-import { TeamsContext, TeamsActivityProcessor } from "botbuilder-teams";
+import { TeamsActivityProcessor } from "botbuilder-teams";
 import AdminCard from "./dialogs/AdminCard";
 import QuickActionCard from "./dialogs/QuickActionsCard";
 import ManagerDashboardCard from "./dialogs/ManagerDashboard";
@@ -41,8 +32,6 @@ const log = debug("msteams");
 @PreventIframe("/acPrototypeBot/acProtoBotTab.html")
 export class AcPrototypeBot implements IBot {
   private readonly conversationState: ConversationState;
-  private readonly dialogs: DialogSet;
-  private dialogState: StatePropertyAccessor<DialogState>;
   private readonly activityProc = new TeamsActivityProcessor();
   private loggedInMemberOIDs: Map<string, object> = new Map();
   /**
@@ -51,95 +40,11 @@ export class AcPrototypeBot implements IBot {
    */
   public constructor(conversationState: ConversationState) {
     this.conversationState = conversationState;
-    this.dialogState = conversationState.createProperty("dialogState");
-    this.dialogs = new DialogSet(this.dialogState);
-    this.dialogs.add(new HelpDialog("help"));
 
     // Set up the Activity processing
-
-    this.activityProc.messageActivityHandler = {
-      // Incoming messages
-      onMessage: async (context: TurnContext): Promise<void> => {
-        // get the Microsoft Teams context, will be undefined if not in Microsoft Teams
-        const teamsContext: TeamsContext = TeamsContext.from(context);
-
-        // TODO: add your own bot logic in here
-        switch (context.activity.type) {
-          case ActivityTypes.Message:
-            const text = teamsContext
-              ? teamsContext.getActivityTextWithoutMentions().toLowerCase()
-              : context.activity.text;
-
-            if (text.startsWith("hello")) {
-              await context.sendActivity("Oh, hello to you as well!");
-              return;
-            } else if (text.startsWith("help")) {
-              const dc = await this.dialogs.createContext(context);
-              await dc.beginDialog("help");
-            } else if (text.includes("card")) {
-              try {
-                const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
-                await context.sendActivity({ attachments: [welcomeCard] });
-              } catch (err) {
-                console.log(err);
-              }
-            } else {
-              console.log(text);
-              await context.sendActivity(
-                `I\'m terribly sorry, but my master hasn\'t trained me to do anything yet...`
-              );
-            }
-            break;
-          case ActivityTypes.Invoke:
-          default:
-            break;
-        }
-
-        // Save state changes
-        return this.conversationState.saveChanges(context);
-      },
-    };
-
-    this.activityProc.conversationUpdateActivityHandler = {
-      onConversationUpdateActivity: async (
-        context: TurnContext
-      ): Promise<void> => {
-        if (
-          context.activity.membersAdded &&
-          context.activity.membersAdded.length !== 0
-        ) {
-          for (const idx in context.activity.membersAdded) {
-            if (
-              context.activity.membersAdded[idx].id ===
-              context.activity.recipient.id
-            ) {
-              const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
-              await context.sendActivity({ attachments: [welcomeCard] });
-            }
-          }
-        }
-      },
-    };
-
-    // Message reactions in Microsoft Teams
-    this.activityProc.messageReactionActivityHandler = {
-      onMessageReaction: async (context: TurnContext): Promise<void> => {
-        const added = context.activity.reactionsAdded;
-        if (added && added[0]) {
-          await context.sendActivity({
-            textFormat: "xml",
-            text: `That was an interesting reaction (<b>${added[0].type}</b>)`,
-          });
-        }
-      },
-    };
-
     this.activityProc.invokeActivityHandler = {
       onInvoke: async (context: TurnContext): Promise<InvokeResponse> => {
         const ctx: any = context;
-        // console.log(ctx.activity);
-        // console.log(ctx.activity.value);
-
         // If not logged in
         if (ctx.activity.value.state != null) {
           this.loggedInMemberOIDs.set(
@@ -313,13 +218,9 @@ export class AcPrototypeBot implements IBot {
       const profile = await response.json();
       return profile.error == null ? profile : undefined;
     } catch (error) {
-      console.log(
-        "***************** Error fetching user profile from graph ***************"
-      );
       log(
         "***************** Error fetching user profile from graph ***************"
       );
-      console.log(error);
       log(error);
     }
   }
